7a7bcb279c692a72c3049421ffd702de0a4281fa
Anusha <anushasg@Anushas-MacBook-Air.local>
2021-06-05 21:55:48 +0530

    Joi added

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const mongoose= require('mongoose');
 const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
+const {campgroundSchema}=require('./Schemas.js')
 
+const validateCampground = (req,res,next) =>{
+    const { error } = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(l=>l.message).join(',');
+        throw new ExpressError(msg,400);
+    }else{
+        next();
+    }
+}
 
 mongoose.connect('mongodb://localhost:27017/camp',{
     useNewUrlParser:true,
@@ -48,8 +59,8 @@ app.get('/campgrounds/addnew',(req,res)=>{
 })
 
 //Using Aysnc
-app.post('/campgrounds',catchAsync( async(req,res,next)=>{
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+app.post('/campgrounds',validateCampground,catchAsync( async(req,res,next)=>{
+    //if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect('/campgrounds');
@@ -79,7 +90,7 @@ app.get('/campgrounds/:id/edit',catchAsync(async(req,res)=>{
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit',{campground});
 }))
-app.put('/campgrounds/:id',catchAsync(async(req,res)=>{
+app.put('/campgrounds/:id',validateCampground,catchAsync(async(req,res)=>{
     const {id} = req.params;
     await Campground.findByIdAndUpdate(id,{...req.body.campground});
     res.redirect(`/campgrounds/${id}`);
