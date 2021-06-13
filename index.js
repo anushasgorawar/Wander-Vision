92f665440c1b1d07f1ce1cf8681e09c694e166a8
Anusha <anushasg@Anushas-MacBook-Air.local>
2021-06-13 16:26:28 +0530

    Added flash

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
 const path = require('path');
 const ejsMate = require('ejs-mate')
 const mongoose= require('mongoose');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const Joi = require('joi');
-const {campgroundSchema}=require('./Schemas.js')
-const {reviewSchema} = require('./Schemas.js')
 const morgan =  require('morgan');
 const methodOverride = require('method-override');
 const campgroundRouter = require('./routes/campground');
 const reviewRouter = require('./routes/review');
 const session = require('express-session')
 const flash= require('connect-flash')
-
-const config = {
-    secret:"Our secret",
-    resave:false ,
-    saveUninitialised : true,
-    cookie : {
-        httpOnly: true,
-        expires: Date.now()+ (1000*60*60*24),
-        maxAge : 1000*60*60*24
-    }
-}
-app.use(session(config));
-app.use(flash());
-
-app.use((req,res,next)=>{
-    res.locals.success = req.flash('success');
-    //res.locals.deleted = req.flash('deleted');
-    // res.locals.error = req.flash('error');
-    next(); 
-})
 
 mongoose.connect('mongodb://localhost:27017/camp',{
     useNewUrlParser:true,
@@ -61,6 +34,27 @@ app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+const config = {
+    secret:"Our secret",
+    resave:false ,
+    saveUninitialised : true,
+    cookie : {
+        httpOnly: true,
+        expires: Date.now()+ (1000*60*60*24),
+        maxAge : 1000*60*60*24
+    }
+}
+app.use(session(config));
+app.use(flash());
+
+app.use((req,res,next)=>{
+    res.locals.success = req.flash('success');
+    console.log("THIS IS PRINTING");
+    res.locals.error = req.flash('error');
+    console.log("THIS IS ALSO PRINTING");
+    next(); 
+})
+
  
 app.use('/campgrounds',campgroundRouter)
 app.use('/campgrounds/:id/review',reviewRouter)
@@ -76,6 +70,10 @@ app.all('*',(req,res,next)=>{
 
 app.use((err,req,res,next)=>{
     const {statusCode = 500 }= err;
+    if(err.kind == 'ObjectId'){
+        req.flash('error',"Campground not found");
+        res.redirect('/campgrounds')
+    }
     if(!err.message ) err.message = "Something Went Wrong";
     res.status(statusCode).render('error',{err});
 
--- routes/campground.js
+++ routes/campground.js
@@ -46,16 +46,31 @@ router.post('/',validateCampground,catchAsync( async(req,res,next)=>{
 
 
 //READ
-router.get('/:id',catchAsync( async(req,res,next)=>{
-    const {id}= req.params;
-    const campground = await Campground.findById(id).populate('reviews');
-    res.render('campgrounds/show',{campground});
-}))
-
+// router.get('/:id',catchAsync( async(req,res,)=>{
+//     const {id}= req.params;
+//     const campground = await Campground.findById(id).populate('reviews');
+//     if(!campground){
+//         req.flash('error','Sorry, That campground doesnt exist');
+//         return res.redirect('/campgrounds');
+//     }
+//     res.render('campgrounds/show',{campground});
+// }))
+router.get('/:id', catchAsync(async (req, res,) => {
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground });
+}));
 //UPDATE
 router.get('/:id/edit',catchAsync(async(req,res)=>{
     const {id} = req.params;
     const campground = await Campground.findById(id);
+    if(!campground){
+        req.flash('error',"Sorry, That campground doesn't exist");
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit',{campground});
 }))
 router.put('/:id',validateCampground,catchAsync(async(req,res)=>{
